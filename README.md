ba02a8034421c3ffd5efa390ea34a728f84e0c3b
António Lima <tocalagarto93@gmail.com>
2016-02-04 16:28:34 +0000

    Greed is ok
    Try to use up all points, starting at page 1 up until page 10.

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ A Python script to automatically enter every giveaway that you can afford on Ste
 The format is pretty straightforward, the program only needs your cookie in order to have access to your SteamGifts session.
 Optionally, you can insert the SteamGifts page number for paging purposes.
 
-    ./steamgifts.py <Cookie> [<SteamGifts Page Number (default=1)>]
+    ./steamgifts.py <Cookie>
     
 In order to obtain your cookie, you can press CTRL+SHIFT+J while on SteamGifts.com (CTRL+SHIFT+K on Firefox) and type document.cookie. Then use the whole string (quotes included) as the argument.
--- steamgifts.py
+++ steamgifts.py
@@ -11,24 +11,30 @@ import re
 MAIN_URL = 'http://www.steamgifts.com'
 PAGING_URL = 'http://www.steamgifts.com/giveaways/search?page='
 USER_AGENT = 'Mozilla/5.0 (X11; Fedora; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36'
-GLOBAL_HEADERS = {'User-Agent': USER_AGENT, 'Accept': 'application/json, text/javascript, */*; q=0.01', 'Accept-encoding': 'gzip, deflate', 'Connection': 'keep-alive', 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+GLOBAL_HEADERS = {'User-Agent': USER_AGENT, 'Accept': 'application/json, text/javascript, */*; q=0.01',
+                  'Accept-encoding': 'gzip, deflate', 'Connection': 'keep-alive',
+                  'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
 MAIN_REGEX = 'href="\/giveaway\/(?P<Code>[a-zA-Z0-9]*?)\/[^\/]*?"'
 XSRF_REGEX = 'name="xsrf_token" value="(?P<XSRF>[0-9a-zA-Z]*?)"'
 
-def getWebPage(url, headers, cookies, postData=None):
+
+def getWebPage(url, headers, cookies, post_data=None):
     try:
-        if (postData):
-            params = urllib.parse.urlencode(postData)
+        if post_data:
+            params = urllib.parse.urlencode(post_data)
             params = params.encode('utf-8')
             request = urllib.request.Request(url, data=params, headers=headers)
         else:
-            print('Fetching '+url)
+            print('Fetching ' + url)
             request = urllib.request.Request(url, None, headers)
+
         request.add_header('Cookie', cookies)
-        if (postData):
+
+        if post_data:
             response = urllib.request.build_opener(urllib.request.HTTPCookieProcessor).open(request)
         else:
             response = urllib.request.urlopen(request)
+
         if response.info().get('Content-Encoding') == 'gzip':
             buf = BytesIO(response.read())
             f = gzip.GzipFile(fileobj=buf)
@@ -37,44 +43,54 @@ def getWebPage(url, headers, cookies, postData=None):
             r = response.read()
 
         return r
+
     except Exception as e:
-        print("Error processing webpage: "+str(e))
+        print("Error processing webpage: " + str(e))
         return None
 
-## https://stackoverflow.com/questions/480214/how-do-you-remove-duplicates-from-a-list-in-python-whilst-preserving-order
+
+# https://stackoverflow.com/questions/480214/how-do-you-remove-duplicates-from-a-list-in-python-whilst-preserving-order
 def nodup(seq):
     seen = set()
     seen_add = seen.add
     return [x for x in seq if not (x in seen or seen_add(x))]
 
+
 def main():
     try:
-        if (len(sys.argv) < 2):
-            print('./steamgifts.py <Cookie> [<SteamGifts Page Number (default=1)>]')
-            print('Please insert your cookie. Press CTRL+SHIFT+J on the website (CTRL+SHIFT+K on Firefox) and type document.cookie. Then paste the whole string as an argument.')
+        if len(sys.argv) < 1:
+            print('./steamgifts.py <Cookie>')
+            print(
+                    'Please insert your cookie. Press CTRL+SHIFT+J on the website (CTRL+SHIFT+K on Firefox) and type document.cookie. Then paste the whole string as an argument.')
             return
+
         regex = re.compile(MAIN_REGEX)
-        xsrfRegex = re.compile(XSRF_REGEX)
+        xsrf_regex = re.compile(XSRF_REGEX)
 
-        if (len(sys.argv) > 2):
-            homePage = getWebPage(PAGING_URL+sys.argv[2], GLOBAL_HEADERS, sys.argv[1])
-        else:
-            homePage = getWebPage(PAGING_URL+'1', GLOBAL_HEADERS, sys.argv[1])
+        for i in range(1, 10):
+            home_page = getWebPage(PAGING_URL + str(i), GLOBAL_HEADERS, sys.argv[1])
 
-        if (homePage is None):
-            print('An error occurred while fetching results (probably expired cookie?). Terminating...')
-            return
+            if home_page is None:
+                print('An error occurred while fetching results (probably expired cookie?). Terminating...')
+                return
 
-        xsrfToken = xsrfRegex.findall(str(homePage))[0]
-        gamesList = regex.findall(str(homePage))
-        gamesList = nodup(gamesList)
-        print(gamesList)
-        for g in gamesList:
-            postData = {'xsrf_token':xsrfToken, 'do':'entry_insert', 'code':g}
-            print(getWebPage(MAIN_URL+'/ajax.php',GLOBAL_HEADERS,sys.argv[1],postData))
+            xsrf_token = xsrf_regex.findall(str(home_page))[0]
+            games_list = regex.findall(str(home_page))
+            games_list = nodup(games_list)
+            print(games_list)
+            for g in games_list:
+
+                post_data = {'xsrf_token': xsrf_token, 'do': 'entry_insert', 'code': g}
+                response_data = getWebPage(MAIN_URL + '/ajax.php', GLOBAL_HEADERS, sys.argv[1], post_data)
+                print(response_data)
+
+                if "\"points\":\"0\"" in response_data.decode('ascii'):
+                    print("0 points left, exiting.")
+                    return
 
     except KeyboardInterrupt:
         print("Interrupted.")
+
 
 if __name__ == '__main__':
     main()
